41bb61f4f3043c124c7dc1584a94692c94dcb4e5
JEFRI SINGH <jefrisingh@gmail.com>
2016-11-04 15:06:31 +0530

    Initial commit

---
+++ app/src/main/java/sundar/ellalan/recyclerview_with_firebase_realtime_database/MainActivity.java
@@ -0,0 +1,13 @@
+package sundar.ellalan.recyclerview_with_firebase_realtime_database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
